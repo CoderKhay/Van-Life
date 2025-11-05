012432f2dd3d3379dd16bd4ab9d23c1bc358a868
Muhammad Kamaldeen <kamaldeenomotolani@gmail.com>
2025-11-05 14:28:06 +0100

    Added the assets folder to hold my images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-red-400 flex justify-center text-4xl font-bold font-serif p-8">
+    <h1 className="text-blue-500 flex justify-center text-4xl font-bold font-serif p-8">
       It's working
     </h1>
   );
