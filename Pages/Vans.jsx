915f006f57b6107f8a2648860589b425ec38be7e
Muhammad Kamaldeen <kamaldeenomotolani@gmail.com>
2025-11-05 23:42:59 +0100

    console throwing an undetected error

---
+++ Pages/Vans.jsx
@@ -0,0 +1,15 @@
+import {useState,useEffect} from "react"
+
+export default function Vans(){
+
+useEffect(() => {
+    fetch("/api/vans")
+    .then(res => res.json())
+    .then(data =>  console.log(data))
+},[])
+
+  return (
+    <div>Vans</div>
+  )
+}
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes , Route, Link} from "react-router-dom";
 import Home from "../Pages/Home"
 import About from "../Pages/About"
+import Vans from "../Pages/Vans"
 
 function App(){
   return(
@@ -9,12 +10,13 @@ function App(){
       <Link className="font-bold text-2xl" to="/">#VANLIFE</Link>
      <nav className="flex gap-5 font-medium">
       <Link to="/About">About</Link>
-      <Link>Vans</Link>
+      <Link to="/Vans">Vans</Link>
     </nav>
  </header>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/About' element={<About/>}></Route>
+      <Route path='/Vans' element={<Vans/>}></Route>
     </Routes>
     </BrowserRouter>
  )
